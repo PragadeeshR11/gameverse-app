16e4e6079961391043dc3acf5627b37cc27e9a0c
PRAGA <r.pragadeesh11@gmail.com>
2026-04-11 23:11:36 +0530

    initial hero section

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from './componenets/Hero'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ src/componenets/Hero.jsx
@@ -0,0 +1,29 @@
+
+
+export default function Hero(){
+    return(
+        <section id='hero'>
+            <div className="header text-amber-300">
+                <div className="logo">
+    
+                </div>
+                <div className="navbar">
+                    <ul>
+                        <li>Latest</li>
+                        <li>Explore</li>
+                        <li>Categories</li>
+                        <li>Subsriptions</li>
+                        <li>Collections</li>
+                        <li>Login/SignUp</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="title">
+                <h2>Gameverse</h2>
+                <p>Interactive interface for discovering and exploring games</p>
+            </div>
+            
+
+        </section>
+    )
+}
