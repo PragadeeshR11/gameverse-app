3176b12511d3827b7faffe05a42f8a690edf993d
PRAGA <r.pragadeesh11@gmail.com>
2026-04-17 21:37:49 +0530

    scroll logic for sticky section

--- src/componenets/Explore.jsx
+++ src/componenets/Explore.jsx
@@ -1,13 +1,14 @@
 import ExploreLayout from '../UI/Explorelayout.jsx';
 import { exploreItems } from '../../gamedata.js';
+import { useState } from 'react';
 
 export default function Explore() {
+  const [activeSec, setActiveSec] = useState(0);
   return (
     <section id="explore" className="h-[300vh]">
       <h2 className="sec-head  audiowide">Xplore and Xperience</h2>
-      {/* <p className="text-gray-400 text-sm mx-6 mt-2"></p> */}
       <div className="sticky h-[80vh] top-0 flex items-center justify-between px-6">
-        <ExploreLayout exploreItem={exploreItems[0]} />
+        <ExploreLayout exploreItem={exploreItems[activeSec]} />
       </div>
     </section>
   );
